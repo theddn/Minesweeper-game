9c07e8224c1bab203b87e2078f1a734f6a815c5b
DimaD30 <82874354+theddn@users.noreply.github.com>
2024-01-01 21:39:55 +0200

    version_2

--- js/game.js
+++ js/game.js
@@ -1,12 +1,9 @@
 'use strict'
 
-const EMPTY = ' '
-const FLOOR = 'FLOOR'
+const EMPTY = ''
 const ROOF = 'ROOF'
-const MINE = 'MINE'
-
-const MINE_IMG = '💣'
-const FLAG_IMG = '🚩'
+const MINE = '💣'
+const FLAG = '🚩'
 
 // The Model
 var gBoard
@@ -41,49 +38,53 @@ function buildBoard() {
             board[i][j] = { minesAroundCount: 0, isShown: false, isMine: false, isMarked: false }
         }
     }
-    board[1][2].isMine = true
-    board[3][0].isMine = true
-    board[3][1].isMine = true
-    // addsMines(board)
+    // board[1][2].isMine = true
+    // board[2][1].isMine = true
+    // board[3][2].isMine = true
+    addsMines(board)
     console.log(board);
     return board
 }
+function addsMines(board) {
+    board[1][2].isMine = true
+    board[2][1].isMine = true
+    board[3][2].isMine = true
+    setMinesNegsCount(board)
 
+}
 function renderBoard(board) {
-    const elBoard = document.querySelector('.board')
     var strHTML = ''
     for (var i = 0; i < board.length; i++) {
         strHTML += '<tr>\n'
         for (var j = 0; j < board[0].length; j++) {
             const currCell = board[i][j]
             var cellClass = getClassName({ i, j })
-            // if (currCell.isShown === true) {
-            //     cellClass += ' floor'
-            // } else { cellClass += ' roof' }
-            strHTML += `\t<td class="cell ${cellClass}" onclick = "onCellClicked(this,${i},${j})">`
-            if (currCell.isMine === true) {
-                strHTML += MINE_IMG
+            strHTML += `\t<td class="cell ${cellClass}"
+            onclick = "onCellClicked(this,${i},${j})">`
+            if(currCell.isMine === true) strHTML += MINE
+            if (currCell.isMine === true)
                 setMinesNegsCount({ i, j }, board)
-            } else {
-                strHTML += EMPTY
-            }
             strHTML += '</td>\n'
         }
         strHTML += '</tr>\n'
     }
+    const elBoard = document.querySelector('.board')
     elBoard.innerHTML = strHTML
 }
-
 
 // TODO: store number in model + dom 
 function setMinesNegsCount(pos, board) {
     var minesCount = 0
+
     for (var i = pos.i - 1; i <= pos.i + 1; i++) {
         if (i < 0 || i >= board.length) continue
+
         for (var j = pos.j - 1; j <= pos.j + 1; j++) {
             if (j < 0 || j >= board[i].length) continue
             if (i === pos.i && j === pos.j) continue
+
             if (board[i][j].isMine === true) minesCount++
+
         }
     }
     console.log(pos, minesCount);
@@ -91,6 +92,7 @@ function setMinesNegsCount(pos, board) {
 }
 
 function onCellClicked(elCell, i, j) {
+    elCell = document.querySelector('.cell')
     const cell = `cell-${i}-${j}`
     console.log(cell);
 }
